d10636ec4394fce86857ad19018c479aa07b77f6
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-04-01 23:13:10 +0200

    init: Set up MAUI app for project park manager mobile
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

---
+++ MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ParkManagerMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
