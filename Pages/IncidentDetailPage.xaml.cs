1e872337270af036d46dad7e63ab3405b0c2002c
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-04-02 19:01:46 +0200

    feat: Create resource incident view page to show incident informations and associated device
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

---
+++ Pages/IncidentDetailPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net.Http.Json;
+using Models;
+
+namespace ParkManagerMobile.Pages
+{
+    public partial class IncidentDetailPage : ContentPage
+    {
+        private readonly HttpClient _httpClient;
+        private readonly Incident _incident;
+
+        public IncidentDetailPage(Incident selectedIncident, HttpClient httpClient)
+        {
+            InitializeComponent();
+            _incident = selectedIncident;
+            _httpClient = httpClient;
+            BindingContext = _incident;
+
+            _ = LoadDeviceInformations();
+        }
+
+        private async Task LoadDeviceInformations()
+        {
+            try
+            {
+                var device = await _httpClient.GetFromJsonAsync<Models.Device>($"devices/{_incident.DeviceId}");
+                
+                if(device != null)
+                {
+                    device.Park = await _httpClient.GetFromJsonAsync<Park>($"parks/{device.ParkId}");
+                    device.Room = await _httpClient.GetFromJsonAsync<Room>($"rooms/{device.RoomId}");
+                }
+
+                DeviceFrame.BindingContext = device;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erreur", ex.Message, "OK");
+            }
+        }
+
+        private async Task ResolveIncident()
+        {
+            try
+            {
+                var response = await _httpClient.PatchAsync($"incidents/{_incident.Id}/close", null);
+                response.EnsureSuccessStatusCode();
+                await Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erreur", ex.Message, "OK");
+            }
+        }
+
+        private async void ResolveIncidentClicked(object sender, EventArgs e)
+        {
+            await ResolveIncident();
+        }
+    }
+
+}
