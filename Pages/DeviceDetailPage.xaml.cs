4c9e1772064eb096160fa4b9db1a5bf4c249077a
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-04-02 02:31:56 +0200

    feat: Create resource device view page to show device informations
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

---
+++ Pages/DeviceDetailPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net.Http.Json;
+using Models;
+
+namespace ParkManagerMobile.Pages
+{
+    public partial class DeviceDetailPage : ContentPage
+    {
+        public DeviceDetailPage(Models.Device selectedDevice)
+        {
+            InitializeComponent();
+            BindingContext = selectedDevice;
+        }
+    }
+
+}
